7c5516fb8326646606b1635858b4ca91c5836072
Lochini <luhradik1@gmail.com>
2025-08-21 23:29:26 +0530

    add initial routes for home, dashboard and nopage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import Dashboard from "./views/Dashboard.jsx";
+import Home from "./views/Home.jsx";
+import NoPage from "./views/NoPage.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p>Hello</p>
-    </>
-  )
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
